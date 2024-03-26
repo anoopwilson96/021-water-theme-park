820826b7e437f7a8822234da22b2172aefc93932
Anoop Wilson <anoopwilson33@yahoo.com>
2024-03-27 00:56:44 +0530

    added images

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function removeIt(){
   const topButton = document.getElementById('topText')
   topButton.remove()
+}
 
+var imgs = document.querySelectorAll('.slider img');
+var dots = document.querySelectorAll('.dot');
+var currentImg = 0; // index of the first image 
+const interval = 3000; // duration(speed) of the slide
 
+function changeSlide(n) {
+  for (var i = 0; i < imgs.length; i++) { // reset
+    imgs[i].style.opacity = 0;
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+
+  currentImg = n;
+
+  imgs[currentImg].style.opacity = 1;
+  dots[currentImg].className += ' active';
 }
