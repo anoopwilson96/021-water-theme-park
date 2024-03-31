65e5adbb326602bc4504d0200068e70371b6b06e
Anoop Wilson <anoopwilson33@yahoo.com>
2024-03-31 15:32:14 +0530

    updated header alert

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+
+// alert above header showing latest offers.
+
 function removeIt(){
   const topButton = document.getElementById('topText')
   topButton.remove()
 }
 
+
 var imgs = document.querySelectorAll('.slider img');
 var dots = document.querySelectorAll('.dot');
 var currentImg = 0; // index of the first image 
-const interval = 3000; // duration(speed) of the slide
+const interval = 3000; // duration(speed) of the slide.
 
 function changeSlide(n) {
   for (var i = 0; i < imgs.length; i++) { // reset
